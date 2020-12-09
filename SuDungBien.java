f195f4c41cc4ae8bcf9b65ca767924b26e85d9fa
manh12a5 <manh12a5@gmail.com>
2020-12-09 14:42:09 +0700

    'Module2'

---
+++ SuDungBien.java
@@ -0,0 +1,17 @@
+public class SuDungBien {
+    public static void main(String[] args) {
+        int a = 10;
+        float b = 20.5f;
+        double c = 20.5;
+        boolean d = true;
+        char e = 'A';
+        String f = "Hello World";
+        System.out.println("int: " + a);
+        System.out.println("float: " + b);
+        System.out.println("double: " + c);
+        System.out.println("boolean: " + d);
+        System.out.println("char: " + e);
+        System.out.println("string: " + f);
+    }
+}
+
